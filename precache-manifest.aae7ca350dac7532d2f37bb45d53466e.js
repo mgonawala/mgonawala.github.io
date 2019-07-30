186b1283a37fc225b58c483fd52e7661d29cc9ef
Mohini Gonawala <mohini.gonawala90@gmail.com>
2019-07-30 22:16:43 +0530

    Updates

---
+++ precache-manifest.aae7ca350dac7532d2f37bb45d53466e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f8f9237fb6577390bd1b928df05106",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "377b4ca3d41538137391",
+    "url": "/react-tutorial/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-tutorial/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "377b4ca3d41538137391",
+    "url": "/react-tutorial/static/js/main.602fa30b.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
